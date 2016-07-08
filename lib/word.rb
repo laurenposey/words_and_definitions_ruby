a83bb04c6b81380fba0a90ea1f30736e46f85b80
Lauren Posey <mrslaurenposey@gmail.com>
2016-07-08 11:08:21 -0700

    add and save word to word list, and clear whenever rspec is ran

--- lib/word.rb
+++ lib/word.rb
@@ -1,9 +1,23 @@
 class Word
+@@word_list = []
+
   define_method(:initialize) do |word_name|
     @word_name = word_name
   end
 
   define_method(:word_name) do
     @word_name
+  end
+
+  define_method(:save) do
+    @@word_list.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@word_list
+  end
+
+  define_singleton_method(:clear) do
+    @@word_list = []
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,11 +2,36 @@ require('rspec')
 require('word')
 
 describe(Word) do
-
+  before() do
+    Word.clear()
+  end
+  
   describe('#word_name') do
    it("returns the word") do
      test_word = Word.new("kitten")
      expect(test_word.word_name()).to(eq("kitten"))
     end
   end
+
+  describe("#save") do
+   it("adds a word to array of words") do
+     test_word = Word.new("kitten")
+     test_word.save()
+     expect(Word.all()).to(eq([test_word]))
+   end
+ end
+
+ describe(".all") do
+    it("is empty at first") do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe(".clear") do
+     it("empties out all of the saved words") do
+       Word.new("kitten").save()
+       Word.clear()
+       expect(Word.all()).to(eq([]))
+     end
+   end
 end
