a33d49b53586d4357a21bfc74fba1c65c97d0181
Lauren Posey <mrslaurenposey@gmail.com>
2016-07-08 11:28:56 -0700

    find a word and return the word's id

--- lib/word.rb
+++ lib/word.rb
@@ -3,6 +3,11 @@ class Word
 
   define_method(:initialize) do |word_name|
     @word_name = word_name
+    @id = @@word_list.length().+(1)
+  end
+
+  define_method(:id) do
+    @id
   end
 
   define_method(:word_name) do
@@ -19,5 +24,15 @@ class Word
 
   define_singleton_method(:clear) do
     @@word_list = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_word = nil
+    @@word_list.each() do |word|
+      if word.id().eql?(id)
+        found_word = word
+      end
+    end
+    found_word
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -5,33 +5,50 @@ describe(Word) do
   before() do
     Word.clear()
   end
-  
+
   describe('#word_name') do
-   it("returns the word") do
-     test_word = Word.new("kitten")
-     expect(test_word.word_name()).to(eq("kitten"))
+    it("returns the word") do
+      test_word = Word.new("kitten")
+      expect(test_word.word_name()).to(eq("kitten"))
     end
   end
 
   describe("#save") do
    it("adds a word to array of words") do
-     test_word = Word.new("kitten")
-     test_word.save()
-     expect(Word.all()).to(eq([test_word]))
-   end
- end
+      test_word = Word.new("kitten")
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
 
- describe(".all") do
+  describe(".all") do
     it("is empty at first") do
       expect(Word.all()).to(eq([]))
     end
   end
 
   describe(".clear") do
-     it("empties out all of the saved words") do
-       Word.new("kitten").save()
-       Word.clear()
-       expect(Word.all()).to(eq([]))
-     end
-   end
+    it("empties out all of the saved words") do
+      Word.new("kitten").save()
+      Word.clear()
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+   describe('#id') do
+    it("returns word id") do
+      test_word = Word.new("kitten")
+      expect(test_word.id()).to(eq(1))
+    end
+  end
+
+  describe(".find") do
+    it("returns word by its id number") do
+      test_word = Word.new("kitten")
+      test_word.save()
+      test_word2 = Word.new("puppy")
+      test_word2.save()
+      expect(Word.find(test_word.id())).to(eq(test_word))
+    end
+  end
 end
